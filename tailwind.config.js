a5a206e8014089cb1dad087b5976c028c77ddc49
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-01-15 14:07:45 +0100

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx})",
+	],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			colors: {
+				orangePrimary: "#A74E12",
+				orangeSecondary: "#DD921D",
+				blackPrimary: "#161513",
+				blackSecondary: "#1C1C22",
+				brownPrimary: "#4D3425",
+				brownSecondary: "#AC8C7D",
+				whitePrimary: "#E6E8EC",
+				whiteSecondary: "#FFFFFF",
+			},
+		},
+		// fontFamily: {
+		// 	reem: "Reem Kufi",
+		// },
+	},
+	plugins: [],
+};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+// 	content: ["./src/**/*.{html,js}"],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	plugins: [],
+// };

7cb6a268202ece815b042c9fecef676c42413525
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-01-15 14:25:52 +0100

    add Layout wrapper with navbar and footer empty components

---
+++ components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+	return (
+		<div className="layout">
+			<Head>
+				<title>Honey_v4</title>
+			</Head>
+			<header>
+				<Navbar />
+			</header>
+			<main>{children}</main>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
+}
+
+export default Layout;

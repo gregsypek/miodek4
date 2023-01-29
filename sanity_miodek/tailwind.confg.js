4efb8cbe501f436b93f719a51b889edbf345e7ac
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-01-29 11:33:01 +0100

    add Sanity as backend and create first schema

---
+++ sanity_miodek/tailwind.confg.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

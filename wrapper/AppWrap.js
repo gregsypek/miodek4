9db7af624086e9b0e004a0f56f26996646748ed7
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-02-15 12:55:27 +0100

    preparing for deployment7(fixing gradients)

--- wrapper/AppWrap.js
+++ wrapper/AppWrap.js
@@ -1,10 +1,13 @@
 import React from "react";
+import styles from '../styles/gradients.module.css'
+
+
 
 const AppWrap = (Component, classNames = null) =>
 	function HOC(props) {
 		// console.log("🚀 ~ file: AppWrap.js:7 ~ HOC ~ props", props);
 		return (
-			<div className={`relative ${classNames}`}>
+			<div className={`relative ${classNames} ${styles.hero}` }>
 				<Component {...props} />
 			</div>
 		);
